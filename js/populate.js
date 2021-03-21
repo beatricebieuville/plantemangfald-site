49dd29c1e27b4f4883e9ed0c113cedafc11355d0
Béatricee <beatrice@skaperkraftvaldres.no>
2021-03-21 18:58:28 +0100

    opprydding:index og planteliste

--- js/populate.js
+++ js/populate.js
@@ -7,11 +7,8 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1RrZrUKaDjsTmtJsS6f2bfUT-J
             if (data.feed.entry[i]['gsx$disponibelt']['$t'] !== '') {
                 var latinsk = data.feed.entry[i]['gsx$latin']['$t'];
                 var norsk = data.feed.entry[i]['gsx$norsk']['$t'];
-                var type = data.feed.entry[i]['gsx$type']['$t'];
-                var mat = data.feed.entry[i]['gsx$mat']['$t'];
-                var oprinnelse = data.feed.entry[i]['gsx$oprinnelse']['$t'];
 
-                document.getElementById('planteliste').innerHTML += ('<tr>'+'<td class="text-capitalize">'+latinsk+'</td>'+'<td class="text-capitalize">'+norsk+'</td>'+'<td class="text-capitalize">'+type+'</td>'+'<td class="text-capitalize">'+mat+'</td>'+'<td class="text-capitalize">'+oprinnelse+'</td>'+'</tr>');
+                document.getElementById('planteliste').innerHTML += ('<tr>'+'<td class="text-capitalize">'+latinsk+'</td>'+'<td class="text-capitalize">'+norsk+'</td>'+'</tr>');
             }
 
         }
